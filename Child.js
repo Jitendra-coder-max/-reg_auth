27071a999844d2276cd28c6a780ff040bd5b013e
Jitendra Nishad <53356706+Jitendra-coder-max@users.noreply.github.com>
2023-06-13 15:13:34 -0400

    Child.js

---
+++ Child.js
@@ -0,0 +1,210 @@
+// // import React from 'react'
+
+// // const Child = (props) => {
+// //   return (
+// //     <div>
+// //         {props.value}
+// //     </div>
+// //   )
+// // }
+
+// // export default Child
+
+
+// import React from 'react'
+
+// const Child = (props) => {
+
+//    const { data } = props
+//   return (
+//     <div>
+//       <h1> Data from parent compo</h1>
+//       <ul>
+//         {/* {data.map((item) => (
+//           <li key={item.id}>{item.name}</li>
+//         ))} */}
+
+//         {
+//           data.map((item)=> (
+//             <li key={item.id }>{item.name}</li>
+//           ))
+//         }
+//       </ul>
+
+//     </div>
+//   )
+// }
+
+// export default Child
+
+
+// import React from 'react'
+
+// const Child = (props) => {
+
+//   const {data} = props
+//   return (
+//     <div>Child
+
+//       <h1>List on name</h1>
+// <ul>
+//    {data.map((item) => (
+//     <li key={item.id}>{item.name}</li>
+//    ))}
+// </ul>
+      
+//     </div>
+//   )
+// }
+
+// export default Child
+
+
+
+// import React from 'react'
+
+// const Child = (props) => {
+
+//   const {data} = props
+//   return (
+//     <div>Child
+
+//       <h1> Data object of array </h1>
+//       <ul>
+//       {data.fruits.map((fruit,index) =>(
+//         <li key={index}>   {index+1} && {fruit}</li>
+//       )
+//       )}
+
+// </ul>
+// <h1>List of flowers</h1>
+// <ul>
+//   {/* {data.flowers.map((index,flower)=>(
+//     <li key={index}>{flower}</li>
+//   )) } */}
+
+//   {data.flowers.map((flower,index)=>
+//   <li key={inde.x}>{flower}  </li>
+//   )}
+// </ul>
+// <h1> List of animal</h1>
+
+// <ul>
+//   {data.animals.map((animal,index) => (
+//     <li key={index}> {animal}</li>
+//   ))}
+// </ul>
+
+
+
+// <ul>
+
+// </ul>
+//     </div>
+//   )
+// }
+
+// export default Child
+
+// import React from 'react'
+
+// const Child = (props) => {
+//   return (
+//     <div>Child   {props.data}  </div>
+//   )
+// }
+
+// export default Child
+
+// import React from 'react'
+
+// const Child = (props) => {
+//   return (
+//     <div>Child
+
+//       {props.data.name}
+//     </div>
+//   )
+// }
+
+// export default Child
+
+// import React from 'react'
+
+// const Child = (props) => {
+
+//   const {data} = props
+//   return (
+//     <div>Child
+
+//       {/* {
+//         props.data
+//       } */}
+
+//       <ul>
+//         {data.map((index) => (
+//           <li> I am  {index}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default Child
+
+// import React from 'react'
+
+// const Child = () => {
+//   return (
+//     <div>Child</div>
+//   )
+// }
+
+// export default Child
+
+
+// import React from 'react'
+
+// const Child = (props) => {
+
+//     const {data} = props
+//   return (
+//     <div>Child
+
+//         <h1>Heading</h1>
+//         <li>
+//             { data.map((item) => (
+//                 <ul key={item.id}>{item.name}</ul>
+//             ))}
+//         </li>
+//     </div>
+//   )
+// }
+
+// export default Child
+
+// import React from 'react'
+
+// const Child = (props) => {
+
+//     const {data} = props
+
+
+//   return (
+//     <div>Child
+//   <h1>Heading</h1>
+//    <ul>
+//     {data.fruits.map((fruit,index)=>
+//     (
+//       <li key ={index}>{fruit}</li>  
+//     ))}
+//    </ul>
+
+
+//     </div>
+//   )
+// }
+
+// export default Child
+
+
